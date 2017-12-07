d51eac29f734feb44eb841947bb75d1fccad11c1
rajeshpeta <rajeshpeta67@gmail.com>
2017-12-07 15:31:34 +0530

    addedscript

---
+++ autoinstall.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+apt-get update
+java -version
+sudo apt-get install default-jre
+sudo apt-get install default-jdk
+sudo apt-get install openjdk-7-jre 
+sudo apt-get install openjdk-7-jdk
+sudo apt-get install python-software-properties
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+sudo update-alternatives --config java
+sudo update-alternatives --config javac
+sudo update-alternatives --config java
+sudo apt-get install maven
+sudo apt-get update
+sudo apt-get install maven
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install git
+sudo apt-get install tree
+sudo apt-get update
+sudo apt-get install rar
+sudo apt-get update
+sudo apt-get install zip
+sudo apt-get install unzip
+
